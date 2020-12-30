4d6cb60c821379e1047ef024ef18132f63f01677
rmazza <robertmazza1@gmail.com>
2020-12-30 13:55:39 -0700

    added getPassengers http.get call to PassengerDashboardService

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,33 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import "rxjs/add/operator/map";
 import { Passenger } from "./models/passenger.interface";
+
+const PASSENGER_API: string = '/api/passengers';
 
 @Injectable()
 export class PassengerDashboardService {
     constructor(private http: Http) {}
 
-    getPassengers(): Passenger[] {
-        return [{
-            id: 1,
-            fullname: 'Stephen',
-            checkedIn: true,
-            checkedInDate: 1490742000000,
-            children: null
-          }, {
-            id: 2,
-            fullname: 'Rose',
-            checkedIn: false,
-            checkedInDate: null,
-            children: [{
-              name: 'Charles',
-              age: 10
-            }]
-          },{
-            id: 3,
-            fullname: 'James',
-            checkedIn: true,
-            checkedInDate: 1490742000000,
-            children: null
-        }];
+    getPassengers(): Observable<Passenger[]> {
+        return this.http
+        .get(PASSENGER_API)
+        .map((response: Response) => response.json());
     }
 }
