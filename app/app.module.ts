afd7243293c313a6646a9e9e568ce3388a14388e
rmazza <robertmazza1@gmail.com>
2021-02-05 08:03:06 -0700

    added Home / Not Found component and nav menu

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,20 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
+import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './not-found.component';
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
     FormsModule,
+    RouterModule.forRoot(routes),
     PassengerDashboardModule
   ],
   bootstrap: [AppComponent]
